26f3e3be90169169ca2e98fe7d823f7517236b8d
Facical <joh0816@gamil.com>
2025-08-11 16:12:21 +0900

    123

---
+++ CarExample/Models/CarParts.swift
@@ -0,0 +1,9 @@
+//
+//  CarParts.swift
+//  CarExample
+//
+//  Created by . on 8/11/25.
+//  Copyright © 2025 Apple. All rights reserved.
+//
+
+import Foundation
---
+++ CarExample/UI/CalloutBubble.swift
@@ -0,0 +1,9 @@
+//
+//  CalloutBubble.swift
+//  CarExample
+//
+//  Created by . on 8/11/25.
+//  Copyright © 2025 Apple. All rights reserved.
+//
+
+import Foundation
